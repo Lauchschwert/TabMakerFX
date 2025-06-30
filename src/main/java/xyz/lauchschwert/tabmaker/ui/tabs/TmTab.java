dadc15954b1f90e179768835101a494c8838c4af
niclas.arnoldner <niclas.arnoldner@stiwa.com>
2025-06-30 10:58:06 +0200

    Rewrite code so the tabs themselfes make new panels

--- src/main/java/xyz/lauchschwert/tabmaker/ui/tabs/TmTab.java
+++ src/main/java/xyz/lauchschwert/tabmaker/ui/tabs/TmTab.java
@@ -1,15 +1,19 @@
 package xyz.lauchschwert.tabmaker.ui.tabs;
 
 import javafx.scene.control.Tab;
+import javafx.scene.layout.VBox;
 import xyz.lauchschwert.tabmaker.ui.panels.TabPanel;
 
 import java.util.List;
 
 public class TmTab extends Tab {
+    private VBox contentPanel;
     private List<TabPanel> tabPanels;
 
     public TmTab(String tabText) {
         super(tabText);
+
+        contentPanel = new VBox();
     }
 
     public void addTabPanel(TabPanel tabPanel) {
