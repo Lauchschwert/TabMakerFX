5e017d4b915fdb415626257b39111c5295566d52
niclas.arnoldner <niclas.arnoldner@stiwa.com>
2025-06-30 08:28:16 +0200

    Prepare Import method
    start working on export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module xyz.lauchschwert.tabmaker {
     requires org.kordamp.ikonli.javafx;
     requires java.desktop;
     requires com.google.gson;
+    requires java.rmi;
 
     opens xyz.lauchschwert.tabmaker to javafx.fxml;
     exports xyz.lauchschwert.tabmaker;
