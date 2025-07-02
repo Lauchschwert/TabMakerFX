84ebd9b4e56114088b13d1229d1b73186001c9f2
niclas.arnoldner <niclas.arnoldner@stiwa.com>
2025-07-02 14:45:36 +0200

    added basic logging to crucial parts of code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,10 @@ module xyz.lauchschwert.tabmaker {
     requires java.desktop;
     requires com.google.gson;
     requires java.rmi;
+    requires java.logging;
 
     opens xyz.lauchschwert.tabmaker to javafx.fxml;
     exports xyz.lauchschwert.tabmaker;
+    exports xyz.lauchschwert.tabmaker.logging;
+    opens xyz.lauchschwert.tabmaker.logging to javafx.fxml;
 }
